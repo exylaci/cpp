d5ffe799e37b89e7a156dd5bab1a3ee741c09dae
exylaci <exy@freemail.hu>
2023-08-14 18:41:38 +0200

    with memorization

--- memorization/main.cpp
+++ memorization/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 
+//with dynamic programming
+int calculated_fibonacci_numbers[1000];
+
 //get Fibonacci number by a recursive function
 int Fibonacci(int n) {
 	if (n == 0 || n == 1) {
 		return n;
 	}
-	return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+	if (calculated_fibonacci_numbers[n] == -1) {
+		calculated_fibonacci_numbers[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+
+	return calculated_fibonacci_numbers[n];
 }
 
 int main() {
-	cout << "Which Fibonacci number do you need: ";
+	cout << "Which Fibonacci number do you need: ";	
 	int n;
 	cin >> n;
+
+	for (int i = 0; i <=n; ++i) {
+		calculated_fibonacci_numbers[i] = -1;
+	}
+
 	int fibonacci_number = Fibonacci(n);
 	cout << "It is: " << fibonacci_number << endl;
 	return 0;
