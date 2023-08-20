73af78c25bef4ae26da99b90d2462e57e88cb3ec
exylaci <exy@freemail.hu>
2023-08-20 19:15:14 +0200

    with for-loop

--- memoizationatrecursive_reducetozero/main.cpp
+++ memoizationatrecursive_reducetozero/main.cpp
@@ -2,6 +2,27 @@
 #include <limits>
 using namespace std;
 
+//for loop (bottom-up) solution
+int CalculateWithFor(int n) {
+	int steps[100000];
+	steps[0] = 0;     //default
+	for (int i = 1; i <= n; ++i) {
+		int a = INT_MAX;
+		int b = INT_MAX;
+		int c = INT_MAX;
+		if (i % 3 == 0) {
+			a = steps[i / 3] + 1;
+		}
+		if (i % 2 == 0) {
+			b = steps[i / 2] + 1;
+		}
+		c = steps[i - 1] + 1;
+
+		steps[i] = min(min(a, b), c);
+	}
+	return steps[n];
+}
+
 //reducting the overlapped recursive calls by memoization
 int calculated_steps[1000000];
 
@@ -37,6 +58,7 @@ int main() {
 
 	fill(calculated_steps, calculated_steps + n + 1, 0);
 
-	cout << "The minimum necessary steps are: " << CalculateSteps(n) << endl;
+	//cout << "The minimum necessary steps are: " << CalculateSteps(n) << endl;
+	cout << "The minimum necessary steps are: " << CalculateWithFor(n) << endl;
 	return 0;
 }
