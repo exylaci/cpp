fc97c188b2bc44d2db06ec17c4068ae54d98e720
exylaci <exy@freemail.hu>
2023-08-24 15:29:05 +0200

    with memoization

--- memoizationrecursive_sellingwines/main.cpp
+++ memoizationrecursive_sellingwines/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//reducting the overlapped recursive calls by memoization
+int calculated_profits[100][100];
+
 //recursive (out-in) solution
 long CalculateProfit(int arr[], int begin, int end, int year) {
 	if (begin > end) {
 		return 0;
 	}
-
+	
+	if (calculated_profits[begin][end] > 0) {
+		return calculated_profits[begin][end];
+	}
+	
 	long left = CalculateProfit(arr, begin + 1, end, year + 1) + year * arr[begin];
 	long right = CalculateProfit(arr, begin, end - 1, year + 1) + year * arr[end];
-
-	return max(left, right);
+	
+	calculated_profits[begin][end] = max(left, right);
+	return calculated_profits[begin][end];
 }
 
 int main() {
@@ -18,9 +26,16 @@ int main() {
 	cout << "How many bottle of wine do you have? ";
 	int n;
 	cin >> n;
+
 	cout << "Give their prices: " << endl;
 	for (int i = 0; i < n; ++i) {
 		cin >> bottles[i];
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			calculated_profits[i][j] = 0;
+		}
 	}
 
 	cout << "The maximum profit is: " << CalculateProfit(bottles, 0, n - 1, 1) << endl;
