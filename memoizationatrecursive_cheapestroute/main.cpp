82f4049a8c2c783ac41f728996efc7470868d8b7
exylaci <exy@freemail.hu>
2023-09-24 20:14:44 +0200

    with memoization

--- memoizationatrecursive_cheapestroute/main.cpp
+++ memoizationatrecursive_cheapestroute/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
 
+//reducting the overlapped recursive calls by memoization
+int calculated_routes[100][100];
+
+//recursive solution:
 int CalculateRoute(int cost[][100], int top, int left, int bottom, int right) {
-	if (left == right && top == bottom)
+	if (calculated_routes[top][left] > -1)
+		return calculated_routes[top][left];
+	if (left == right && top == bottom) {
+		calculated_routes[top][left] = cost[bottom][right];
 		return cost[bottom][right];
-	if (left == right)
-		return cost[top][left] + CalculateRoute(cost, top + 1, left, bottom, right);
-	if (top == bottom)
-		return cost[top][left] + CalculateRoute(cost, top, left + 1, bottom, right);
-	return cost[top][left] + min(
-		CalculateRoute(cost, top, left + 1, bottom, right),
-		CalculateRoute(cost, top + 1, left, bottom, right)
-	);
+	}
+	if (left == right) {
+		calculated_routes[top][left] =
+			cost[top][left] + CalculateRoute(cost, top + 1, left, bottom, right);
+		return calculated_routes[top][left];
+	}
+	if (top == bottom) {
+		calculated_routes[top][left] =
+			cost[top][left] + CalculateRoute(cost, top, left + 1, bottom, right);
+		return calculated_routes[top][left];
+	}
+	calculated_routes[top][left] =
+		cost[top][left] + min(
+			CalculateRoute(cost, top, left + 1, bottom, right),
+			CalculateRoute(cost, top + 1, left, bottom, right)
+		);
+	return calculated_routes[top][left];
 }
 
 int main() {
@@ -26,6 +42,7 @@ int main() {
 	for (int i = 0; i < r; ++i) {
 		for (int j = 0; j < c; ++j) {
 			cin >> costs[i][j];
+			calculated_routes[i][j] = -1;
 		}
 	}
 
