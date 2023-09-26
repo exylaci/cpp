ffbb26e7bb4f6a4c87276f7519f48e3aca56cb60
exylaci <exy@freemail.hu>
2023-09-26 07:59:23 +0200

    with memoization

--- memoizationatrecursive_mostexpensivesplit/main.cpp
+++ memoizationatrecursive_mostexpensivesplit/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//reducting the overlapped recursive calls by memoization
+int calculated_costs[100];
+
 //recursive solution:
 int CalculateSplits(int costs[], int length) {
 	if (length == 0)return 0;
+	if (calculated_costs[length] > -1) {
+		return calculated_costs[length];
+	}
+
 	int resoult = 0;
 	for (int i = 1; i <= length; ++i) {
 		resoult = max(resoult, costs[i] + CalculateSplits(costs, length - i));
@@ -19,6 +26,7 @@ int main() {
 	for (int i = 1; i <= length; ++i) {
 		cout << "Give the costs of the " << i << " length section: ";
 		cin >> costs[i];
+		calculated_costs[i] = -1;
 	}
 
 	cout << "The most expensive splits is: " << CalculateSplits(costs, length) << endl;
