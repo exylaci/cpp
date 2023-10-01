6be23137669f4f4755761a48be7fbeee76656869
exylaci <exy@freemail.hu>
2023-10-01 21:59:52 +0200

    project: Gödrök - 2nd exercise

--- erettsegiemelt2021majus_4godrok/main.cpp
+++ erettsegiemelt2021majus_4godrok/main.cpp
@@ -20,8 +20,29 @@ void feladat1() {
 	cout << "A fájl adatainak száma: " << deeps.size() << endl << endl;
 }
 
+int feladat2() {
+	using namespace std;
+	cout << "2. feladat" << endl;
+
+	cout << "Adjon meg egy távolságértéket! ";
+	int distance;
+	cin >> distance;
+
+	if (distance < 1 || distance > deeps.size()) {
+		cout << "Nem megfelelõ a megadott távolság!" << endl << endl;
+	}
+	else {
+		cout << "Ezen a helyen a felszín " <<
+			deeps.at(distance - 1)
+			<< " méter mélyen van." << endl << endl;
+	}
+
+	return distance;
+}
+
 int main() {
 	SetConsoleOutputCP(1250);
 	feladat1();
+	int distance{ feladat2() };
 	return 0;
 }
