0c9401bfb3902972fa096204e5fdf509ecaa2856
exylaci <exy@freemail.hu>
2023-10-13 21:44:37 +0200

    project: Gödrök - 6th exercise

--- erettsegiemelt2021majus_4godrok/main.cpp
+++ erettsegiemelt2021majus_4godrok/main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <iomanip>
 
-
 std::vector<int> deeps{};
 
 void feladat1() {
@@ -47,7 +46,7 @@ void feladat3() {
 	cout << "3. feladat" << endl;
 
 	int pieces{ 0 };
-	for (const auto &deep : deeps) {
+	for (const auto& deep : deeps) {
 		if (deep == 0) { ++pieces; }
 	}
 
@@ -66,7 +65,7 @@ void feladat4() {
 	bool newline{ false };
 
 	std::ofstream fs{ "godrok.txt" };
-	for (const auto &deep : deeps) {
+	for (const auto& deep : deeps) {
 		if (deep == 0 && newline) {
 			fs << std::endl;
 			newline = false;
@@ -102,6 +101,65 @@ void feladat5() {
 	cout << "A gödrök száma: " << pieces << endl << endl;
 }
 
+void feladat6(int distance) {
+	using namespace std;
+	cout << "6. feladat" << endl;
+
+	if (deeps.at(distance - 1) == 0) {
+		cout << "Az adott helyen nincs gödör." << endl << endl;
+		return;
+	}
+
+	int first{ distance - 1 };
+	while (first > 0 && deeps.at(first - 1) > 0) {
+		--first;
+	}
+	cout << "a)" << endl;
+	cout << "A gödör kezdete: " << first + 1 << " méter, ";
+
+	int last{ distance - 1 };
+	int size{ (int)deeps.size() - 1 };
+	while (last < size && deeps.at(last + 1) > 0) {
+		++last;
+	}
+	cout << "a gödör vége: " << last + 1 << " méter." << endl;
+
+	enum DIRECTION { DOWN, UP, NONMONOTIC };
+	DIRECTION direction{ DOWN };
+	int maxDeep{ 0 };
+	int totalDeep{ 0 };
+	int safeTotalDeep{ 0 };
+
+	for (int position = first; position <= last; ++position) {
+		if (first > 0 && direction == DOWN && deeps.at(position - 1) < deeps.at(position)) {
+			direction = UP;
+		}
+		if (first > 0 && direction == UP && deeps.at(position - 1) > deeps.at(position)) {
+			direction = NONMONOTIC;
+		}
+		maxDeep = max(maxDeep, deeps.at(position));
+		totalDeep += deeps.at(position);
+		safeTotalDeep += deeps.at(position) - 1;
+	}
+
+	cout << "b)" << endl;
+	if (direction == NONMONOTIC) {
+		cout << "Nem mélyül folyamatosan." << endl;
+	}
+	else {
+		cout << "Folyamatosan mélyül." << endl;
+	}
+
+	cout << "c)" << endl << "A legnagyobb mélysége " <<
+		maxDeep << " méter." << endl;
+
+	cout << "d)" << endl << "A térfogata " <<
+		totalDeep * 10 << " m^3." << endl;
+
+	cout << "e)" << endl << "A vízmennyiség " <<
+		safeTotalDeep * 10 << " m^3." << endl << endl;
+}
+
 int main() {
 	SetConsoleOutputCP(1250);
 	feladat1();
@@ -109,5 +167,6 @@ int main() {
 	feladat3();
 	feladat4();
 	feladat5();
+	feladat6(distance);
 	return 0;
 }
