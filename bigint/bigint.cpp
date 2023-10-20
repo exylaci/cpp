3279d233b6a0e83da9cadff873380fbc4b572a77
exylaci <exy@freemail.hu>
2023-10-20 18:32:33 +0200

    BigInteger - optimization

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -1,6 +1,8 @@
-// C++ program to implement
-// the above approach
-#include <bits/stdc++.h>
+// C++ program to implement calculating with extra long, Big Integer numbers
+//#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -440,75 +442,75 @@ ostream& operator<<(ostream& out, const BigInt& a) {
 }
 
 //Driver code with some examples
-int main()
-{
-	BigInt first("12345");
-	cout << "The number of digits"
-		<< " in first big integer = "
-		<< Length(first) << '\n';
-	BigInt second(12345);
-	if (first == second) {
-		cout << "first and second are equal!\n";
-	}
-	else
-		cout << "Not equal!\n";
-	BigInt third("10000");
-	BigInt fourth("100000");
-	if (third < fourth) {
-		cout << "third is smaller than fourth!\n";
-	}
-	BigInt fifth("10000000");
-	if (fifth > fourth) {
-		cout << "fifth is larger than fourth!\n";
-	}
-
-	// Printing all the numbers
-	cout << "first = " << first << '\n';
-	cout << "second = " << second << '\n';
-	cout << "third = " << third << '\n';
-	cout << "fourth = " << fourth << '\n';
-	cout << "fifth = " << fifth << '\n';
-
-	// Incrementing the value of first
-	first++;
-	cout << "After incrementing the"
-		<< " value of first is : ";
-	cout << first << '\n';
-	BigInt sum;
-	sum = (fourth + fifth);
-	cout << "Sum of fourth and fifth = "
-		<< sum << '\n';
-	BigInt product;
-	product = second * third;
-	cout << "Product of second and third = "
-		<< product << '\n';
-
-	// Print the fibonacci number from 1 to 100
-	cout << "-------------------------Fibonacci"
-		<< "------------------------------\n";
-	for (int i = 0; i <= 100; i++) {
-		BigInt Fib;
-		Fib = NthFibonacci(i);
-		cout << "Fibonacci " << i << " = " << Fib << '\n';
-	}
-	cout << "-------------------------Catalan"
-		<< "------------------------------\n";
-	for (int i = 0; i <= 100; i++) {
-		BigInt Cat;
-		Cat = NthCatalan(i);
-		cout << "Catalan " << i << " = " << Cat << '\n';
-	}
-
-	// Calculating factorial of from 1 to 100
-	cout << "-------------------------Factorial"
-		<< "------------------------------\n";
-	for (int i = 0; i <= 100; i++) {
-		BigInt fact;
-		fact = Factorial(i);
-		cout << "Factorial of "
-			<< i << " = ";
-		cout << fact << '\n';
-	}
-	// This code is contributed
-	// by Gatea David
-}
+//int main()
+//{
+//	BigInt first("12345");
+//	cout << "The number of digits"
+//		<< " in first big integer = "
+//		<< Length(first) << '\n';
+//	BigInt second(12345);
+//	if (first == second) {
+//		cout << "first and second are equal!\n";
+//	}
+//	else
+//		cout << "Not equal!\n";
+//	BigInt third("10000");
+//	BigInt fourth("100000");
+//	if (third < fourth) {
+//		cout << "third is smaller than fourth!\n";
+//	}
+//	BigInt fifth("10000000");
+//	if (fifth > fourth) {
+//		cout << "fifth is larger than fourth!\n";
+//	}
+//
+//	// Printing all the numbers
+//	cout << "first = " << first << '\n';
+//	cout << "second = " << second << '\n';
+//	cout << "third = " << third << '\n';
+//	cout << "fourth = " << fourth << '\n';
+//	cout << "fifth = " << fifth << '\n';
+//
+//	// Incrementing the value of first
+//	first++;
+//	cout << "After incrementing the"
+//		<< " value of first is : ";
+//	cout << first << '\n';
+//	BigInt sum;
+//	sum = (fourth + fifth);
+//	cout << "Sum of fourth and fifth = "
+//		<< sum << '\n';
+//	BigInt product;
+//	product = second * third;
+//	cout << "Product of second and third = "
+//		<< product << '\n';
+//
+//	// Print the fibonacci number from 1 to 100
+//	cout << "-------------------------Fibonacci"
+//		<< "------------------------------\n";
+//	for (int i = 0; i <= 100; i++) {
+//		BigInt Fib;
+//		Fib = NthFibonacci(i);
+//		cout << "Fibonacci " << i << " = " << Fib << '\n';
+//	}
+//	cout << "-------------------------Catalan"
+//		<< "------------------------------\n";
+//	for (int i = 0; i <= 100; i++) {
+//		BigInt Cat;
+//		Cat = NthCatalan(i);
+//		cout << "Catalan " << i << " = " << Cat << '\n';
+//	}
+//
+//	// Calculating factorial of from 1 to 100
+//	cout << "-------------------------Factorial"
+//		<< "------------------------------\n";
+//	for (int i = 0; i <= 100; i++) {
+//		BigInt fact;
+//		fact = Factorial(i);
+//		cout << "Factorial of "
+//			<< i << " = ";
+//		cout << fact << '\n';
+//	}
+//	// This code is contributed
+//	// by Gatea David
+//}
