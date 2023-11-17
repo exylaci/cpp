8c2bcdf1828c7865a01bbc135ac1132a8a79e6e4
exylaci <exy@freemail.hu>
2023-11-17 13:06:12 +0100

    algorithm: count independent graphs - update: there are islands without road to their only one city!

--- algorithm_CountIndependentGraphs/main.cpp
+++ algorithm_CountIndependentGraphs/main.cpp
@@ -21,80 +21,87 @@ int main() {
 		int cities;
 		fi >> cities;
 
-		std::list<std::list<int>> islands{};
+		//std::list<std::list<int>> islands{};
+		//int city1;
+		//int city2;
+		//while (fi >> city1 >> city2) {
+		//	auto island1 = islands.end();
+		//	auto island2 = islands.end();
+		//	for (auto it = islands.begin(); it != islands.end(); ++it) {
+		//		if (island1 == islands.end()) {
+		//			if (std::find((*it).begin(), (*it).end(), city1) != (*it).end()) {
+		//				island1 = it;
+		//			}
+		//		}
+		//		if (island2 == islands.end()) {
+		//			if (std::find((*it).begin(), (*it).end(), city2) != (*it).end()) {
+		//				island2 = it;
+		//			}
+		//		}
+		//		if (island1 != islands.end() && island2 != islands.end()) {
+		//			break;
+		//		}
+		//	}
+		//	if (island1 == islands.end() && island2 == islands.end()) {
+		//		islands.emplace_back(std::list<int> {city1, city2});
+		//	}
+		//	else if (island1 == islands.end()) {
+		//		(*island2).emplace_back(city1);
+		//	}
+		//	else if (island2 == islands.end()) {
+		//		(*island1).emplace_back(city2);
+		//	}
+		//	else if (island1 != island2) {
+		//		std::copy((*island2).begin(), (*island2).end(), std::back_inserter((*island1)));
+		//		islands.erase(island2);
+		//	}
+		//}
+
+		int* a = new int[cities];
+		for (int index = 0; index < cities; ++index) a[index] = 0;
+
 		int city1;
 		int city2;
+		int current;
+		int merge;
+		int islands{ 0 };
+		int counter{ 1 };
 		while (fi >> city1 >> city2) {
-			auto island1 = islands.end();
-			auto island2 = islands.end();
-			for (auto it = islands.begin(); it != islands.end(); ++it) {
-				if (island1 == islands.end()) {
-					if (std::find((*it).begin(), (*it).end(), city1) != (*it).end()) {
-						island1 = it;
+			if (a[city1] == 0 && a[city2] == 0) {
+				a[city1] = counter;
+				a[city2] = counter;
+				++islands;
+				++counter;
+			}
+			else {
+				if (a[city1] == 0) {
+					a[city1] = a[city2];
+				}
+				else if (a[city2] == 0) {
+					a[city2] = a[city1];
+				}
+				else if (a[city1] != a[city2]) {
+					current = std::min(a[city1], a[city2]);
+					merge = std::max(a[city1], a[city2]);
+					for (int index = 0; index < cities; ++index) {
+						if (a[index] == merge) {
+							a[index] = current;
+						}
 					}
-				}
-				if (island2 == islands.end()) {
-					if (std::find((*it).begin(), (*it).end(), city2) != (*it).end()) {
-						island2 = it;
-					}
-				}
-				if (island1 != islands.end() && island2 != islands.end()) {
-					break;
+					--islands;
 				}
 			}
-			if (island1 == islands.end() && island2 == islands.end()) {
-				islands.emplace_back(std::list<int> {city1, city2});
-			}
-			else if (island1 == islands.end()) {
-				(*island2).emplace_back(city1);
-			}
-			else if (island2 == islands.end()) {
-				(*island1).emplace_back(city2);
-			}
-			else if (island1 != island2) {
-				std::copy((*island2).begin(), (*island2).end(), std::back_inserter((*island1)));
-				islands.erase(island2);
+		}
+		fi.close();
+
+		//adding the islands without roads to their only one piece city
+		for (int index = 0; index < cities; ++index) {
+			if (a[index] == 0) {
+				++islands;
 			}
 		}
 
-		//int* a = new int[cities];
-		//for (int index = 0; index < cities; ++index) a[index] = 0;
-
-		//int city1;
-		//int city2;
-		//int current;
-		//int merge;
-		//int islands{ 0 };
-		//int counter{ 1 };
-		//while (fi >> city1 >> city2) {
-		//	if (a[city1] == 0 && a[city2] == 0) {
-		//		a[city1] = counter;
-		//		a[city2] = counter;
-		//		++islands;
-		//		++counter;
-		//	}
-		//	else {
-		//		if (a[city1] == 0) {
-		//			a[city1] = a[city2];
-		//		}
-		//		else if (a[city2] == 0) {
-		//			a[city2] = a[city1];
-		//		}
-		//		else if (a[city1] != a[city2]) {
-		//			current = std::min(a[city1], a[city2]);
-		//			merge = std::max(a[city1], a[city2]);
-		//			for (int index = 0; index < cities; ++index) {
-		//				if (a[index] == merge) {
-		//					a[index] = current;
-		//				}
-		//			}
-		//			--islands;
-		//		}
-		//	}
-		//}
-		fi.close();
-
-		std::cout << fileCounter << ":  szigetek szama: " << islands.size();
-		std::cout << "    (futasi ido:" << std::setw(5) << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count() << ")" << std::endl;
+		std::cout << fileCounter << ":  szigetek szama: " << std::setw(5) << islands;
+		std::cout << "    (futasi ido:" << std::setw(6) << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count() << " millisec)" << std::endl;
 	}
 }
