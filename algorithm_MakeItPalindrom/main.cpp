7684e280c7e2cb6add0bde64122cab4eefc4329b
exylaci <exy@freemail.hu>
2023-11-28 16:12:30 +0100

    algorithm: make it palindrom - task 2

--- algorithm_MakeItPalindrom/main.cpp
+++ algorithm_MakeItPalindrom/main.cpp
@@ -18,9 +18,15 @@ int task2(std::vector<int> test_case) {
 	return 0;
 }
 
+//Count the difference from the beginnig or end, the fewer ones.
 int task3(std::vector<int> test_case) {
-	// TODO: Task 3 solution here
-	return 0;
+	if (test_case.at(0) == test_case.at(test_case.size() - 1)) return 0;
+	for (int i = 1; i < test_case.size(); ++i) {
+		if (test_case.at(i) != test_case.at(i - 1) ||
+			test_case.at(test_case.size() - 1) != test_case.at(test_case.size() - 1 - i)) {
+			return(i);
+		}
+	}
 }
 
 int main() {
