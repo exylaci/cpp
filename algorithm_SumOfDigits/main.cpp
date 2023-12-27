59673936b86bef16f5628b337865843f50bea50b
exylaci <exy@freemail.hu>
2023-12-27 10:14:46 +0100

    algorithm: sum of digits - load data from input files

--- algorithm_SumOfDigits/main.cpp
+++ algorithm_SumOfDigits/main.cpp
@@ -3,11 +3,21 @@
 
 int main() {
 	for (char fileCounter{ '1' }; fileCounter <= '2'; ++fileCounter) {
-		auto fh = openInputFile(fileCounter);
-		fh.close();
+		std::string first;
+		std::string last;
+		int sum;
+		loadDataFromInputfile(fileCounter, first, last, sum);
 	}
 }
 
+//load min, max, summary data from inout files
+void loadDataFromInputfile(const char& order, std::string& first, std::string& last, int& sum) {
+	auto fileHandler = openInputFile(order);
+	if (fileHandler) {
+		fileHandler >> first >> last >> sum;
+		fileHandler.close();
+	}
+}
 //open the input file for reading
 std::ifstream openInputFile(const char& order) {
 	auto filename{ prepareFileName(order) };
--- algorithm_SumOfDigits/main.h
+++ algorithm_SumOfDigits/main.h
@@ -3,6 +3,7 @@
 
 const std::string BEGINNING_OF_INPUT_FILE_NAME = "osszeg.pelda";
 
+void loadDataFromInputfile(const char& order, std::string& first, std::string& last, int& sum);
 std::ifstream openInputFile(const char& order);
 std::string prepareFileName(const char& order);
 std::ifstream openFile(const std::string& filename);
