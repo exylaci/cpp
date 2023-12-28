88e6de1e8440cf3c83091c92c1a5774bb0b3c897
exylaci <exy@freemail.hu>
2023-12-28 10:55:27 +0100

    algorithm: sum of digits - casee: initial sum is less than the requested

--- algorithm_SumOfDigits/main.cpp
+++ algorithm_SumOfDigits/main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 
 int main() {
-	for (char fileCounter{ '1' }; fileCounter <= '2'; ++fileCounter) {
+	for (char fileCounter{ '0' }; fileCounter <= '7'; ++fileCounter) {
 		std::string first;
 		std::string last;
 		int sum;
 		loadDataFromInputfile(fileCounter, first, last, sum);
+		auto resoult{ findTheNumber(first,last, sum) };
+		printResoult(fileCounter, resoult, last);
 	}
 }
 
@@ -28,7 +30,7 @@ std::ifstream openInputFile(const char& order) {
 std::string prepareFileName(const char& order) {
 	std::string filename(BEGINNING_OF_INPUT_FILE_NAME);
 	filename += order;
-	filename += ".in.txt";
+	filename += END_OF_INPUT_FILE_NAME;
 	return filename;
 }
 //open the input file for reading
@@ -39,4 +41,72 @@ std::ifstream openFile(const std::string& filename) {
 		cout << filename << "    <--- Baj van! Nem nyilt meg." << endl;
 	}
 	return fileHandler;
+}
+
+//find the the first number that is greather than the <<first>> and the sum of its digit is equal to <<sum>>
+std::string findTheNumber(std::string& first, std::string& last, const int& sum) {
+	auto differences{ calculateDifference(sumOfDigits(first), sum) };
+	std::string resoult{};
+	if (differences < 0) {
+		resoult = sumIsLess(first, differences);
+	}
+	else {
+		resoult = sumIsGreather(first, differences);
+	}
+	return validateResoult(resoult, last);
+}
+//summarize the values of the digits
+int sumOfDigits(std::string& numberString) {
+	int resoult{ 0 };
+	for (int index = 0; index < numberString.length(); ++index) {
+		resoult += numberString[index] - '0';
+	}
+	return resoult;
+}
+//calculate the difference between the first and the second parameter
+int calculateDifference(int one, int two) {
+	return two - one;
+}
+//sum is greather -> last digits should be increased 
+const std::string& sumIsGreather(std::string& numberString, int& differences) {
+	int index = numberString.length();
+	while (differences > 0) {
+		if (--index < 0) {
+			index = 0;
+			numberString = '0' + numberString;
+		}
+		auto diff = '9' - numberString[index];
+		if (diff > differences) {
+			numberString[index] += differences;
+		}
+		else {
+			numberString[index] += diff;
+		}
+		differences -= diff;
+	}
+	return numberString;
+}
+
+//sum is less -> last digits should be decreased 
+std::string sumIsLess(std::string& numberString, int& differences) {
+	return "0";
+}
+
+//check whether the resoult is greather than the secound input number
+std::string validateResoult(std::string& resoult, std::string& last) {
+	if (isItGreater(resoult, last)) {
+		return "NINCS";
+	}
+	return resoult;
+}
+//check whether the resoult is greather than the secound input number
+bool isItGreater(const BigInt one, const BigInt two) {
+	return one > two;
+}
+//print out the resoult
+void printResoult(const char& fileCounter, const std::string& resoult, const std::string& last) {
+	std::cout
+		<< fileCounter << ": "
+		<< resoult
+		<< std::endl;
 }
--- algorithm_SumOfDigits/main.h
+++ algorithm_SumOfDigits/main.h
@@ -1,9 +1,20 @@
 #pragma once
 #include <fstream>
+#include "../bigint/bigint.cpp"
 
-const std::string BEGINNING_OF_INPUT_FILE_NAME = "osszeg.pelda";
+const std::string BEGINNING_OF_INPUT_FILE_NAME = "osszeg";
+const std::string END_OF_INPUT_FILE_NAME = ".in.txt";
 
 void loadDataFromInputfile(const char& order, std::string& first, std::string& last, int& sum);
 std::ifstream openInputFile(const char& order);
 std::string prepareFileName(const char& order);
 std::ifstream openFile(const std::string& filename);
+
+std::string findTheNumber(std::string& first, std::string& last, const int& sum);
+int sumOfDigits(std::string& numberString);
+int calculateDifference(int one, int two);
+const std::string& sumIsGreather(std::string& numberString, int& differences);
+std::string sumIsLess(std::string& numberString, int& differences);
+std::string validateResoult(std::string& resoult, std::string& last);
+bool isItGreater(const BigInt one, const BigInt two);
+void printResoult(const char& fileCounter, const std::string& resoult, const std::string& last);
